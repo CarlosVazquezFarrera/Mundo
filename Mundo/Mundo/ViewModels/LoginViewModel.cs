0c2d0797a69fc9d4eb93825b3aa526eb30729ecc
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2019-03-17 17:25:04 -0600

    LoginViewModel

---
+++ Mundo/Mundo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Mundo.ViewModels
+{
+    using System.Windows.Input;
+    public class LoginViewModel
+    {
+        #region Propiedades
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemember { get; set; }
+        #endregion
+
+        #region constructores
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+        }
+        #endregion
+
+        #region Comandos
+        public ICommand LoginCommand { get; set; }
+        #endregion
+    }
+}
--- Mundo/Mundo/ViewModels/MainViewModel.cs
+++ Mundo/Mundo/ViewModels/MainViewModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Mundo.ViewModels
+﻿namespace Mundo.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructores
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
